ec96b5f4b4932e9be533f32d535f0f5b20e8e5f6
Sheila Sharon <devsheilawambui@gmail.com>
2023-06-23 09:36:10 +0300

    Edit next config

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
     GOOGLE_MAPS_API_KEY: "AIzaSyB68BIJkMeQLuHc2eXW5UOVGnzIPKfs7Hg",
     TRIP_ADVISOR_API_KEY:"0ad5345e3cmsh0252cb9761788fep1c8b4fjsnd3a3740b38c1"
   },
+  eslint: { ignoreDuringBuilds: true } 
 };
 
 module.exports = nextConfig;
